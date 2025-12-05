4c78aef0cff79c51b527afec57ba02ebaf331fb8
Yash Samrat <yashsamrat10080@gmail.com>
2025-12-05 18:30:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 # Developed by @oxeign
 import os
 
-API_ID = int(os.environ.get("API_ID") or 0)      # set env var API_ID to avoid editing file
-API_HASH = os.environ.get("API_HASH") or ""     # set env var API_HASH
+API_ID = int(os.environ.get("24090342") or 0)      # set env var API_ID to avoid editing file
+API_HASH = os.environ.get("b1bc1341aac0d5cea8da11872969142f") or ""     # set env var API_HASH
 DEVELOPER = "@oxeign"
 
 if API_ID == 0 or API_HASH == "":
