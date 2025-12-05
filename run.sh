2753b3d95d4403fab9d1679f711134813d1b62bb
Yash Samrat <yashsamrat10080@gmail.com>
2025-12-05 19:57:01 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# run.sh — launcher for Termux
-# Make executable: chmod +x run.sh
-echo "Starting OxyReaction (Termux) - developed by @oxeign"
-python3 main.py
